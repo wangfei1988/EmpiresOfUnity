5ff7778370416467a94ce83a12ec38ab4b216f85
LucasSuperPower <lucas.n@live.de>
2014-05-19 17:40:18 +0200

    Better Menu

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,35 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenu : MonoBehaviour 
+public class MainMenu : MonoBehaviour
 {
-
-	void Start ()
-	{
-	    UpdateManager.OnUpdate += this.DoUpdate;
-	}
+  
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5, Screen.width / 3, Screen.height / 5), "Spiel Starten"))
-        {
-            Debug.Log("Start");
-            Application.LoadLevel("InGame");
-        }
-        if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5*2, Screen.width / 3, Screen.height / 5), "Ende"))
-        {
-            Debug.Log("Ende");
-            Application.Quit();
-        }
+         if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5, Screen.width / 3, Screen.height / 5), "Start"))
+         {
+             Application.LoadLevel("InGame");
+         }
+         if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5 * 2, Screen.width / 3, Screen.height / 5), "Settings"))
+         {
+             Application.LoadLevel("Settings");
+         }
+         if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5 * 3, Screen.width / 3, Screen.height / 5), "Exit"))
+         {
+             Application.Quit();
+         }
     }
 
-    void DoUpdate()
-    {
-
-    }
-
-    void OnDestroy()
-    {
-        UpdateManager.OnUpdate -= this.DoUpdate;
-    }
 }
---
+++ Assets/Scripts/Menus/Settings.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour 
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5, Screen.width / 3, Screen.height / 5), "Credits"))
+        {
+        }
+        if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5 * 2, Screen.width / 3, Screen.height / 5), "Developer Homepage"))
+        {
+            Application.OpenURL("http://media.giphy.com/media/gU25raLP4pUu4/giphy.gif");
+        }
+        if (GUI.Button(new Rect(Screen.width / 3, Screen.height / 5 * 3, Screen.width / 3, Screen.height / 5), "Back"))
+        {
+            Application.LoadLevel("MainMenu");
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
