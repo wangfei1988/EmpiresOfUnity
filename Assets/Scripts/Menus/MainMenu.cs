5eda30be04204dee53091bc6c3f7c1f549266a4d
Dario D. Müller <dario-domi@hotmail.de>
2014-06-09 16:49:56 +0200

    Menu Background

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,24 +5,34 @@ public class MainMenu : MonoBehaviour
 
     public GameObject Controls;
     public GameObject Credits;
+    
+    private GUIStyle style = new GUIStyle();
+    public Texture2D background;
+
+
+    void Start()
+    {
+        style.normal.background = background;
+        style.fontSize = 20;
+        style.alignment = TextAnchor.MiddleCenter;
+    }
 
     void OnGUI()
     {
         Vector2 size = new Vector2(1920, 1080);
 
         //float left = Screen.width / 2 - Screen.width / 4;
-        float left = Screen.width/size.x*1150f;
+        float left = Screen.width/size.x*1300f;
         float top = Screen.height / size.y * 300f;
-        float topPlus = Screen.width / size.x * 100f;
+        float topPlus = Screen.width / size.x * 80f;
         float width = Screen.width / size.x * 400f;
-        float height = Screen.width / size.x * 80f;
+        float height = Screen.width / size.x * 60f;
 
-
-        if (GUI.Button(new Rect(left, top + topPlus *1, width, height), "Start"))
+        if (GUI.Button(new Rect(left, top + topPlus *1, width, height), "Start", style))
         {
             Application.LoadLevel("InGame");
         }
-        if (GUI.Button(new Rect(left, top + topPlus * 2, width, height), "Controls"))
+        if (GUI.Button(new Rect(left, top + topPlus * 2, width, height), "Controls", style))
         {
             Credits.renderer.enabled = false;
             if (Controls.renderer.isVisible)
@@ -30,7 +40,7 @@ public class MainMenu : MonoBehaviour
             else
                 Controls.renderer.enabled = true;
         }
-        if (GUI.Button(new Rect(left, top + topPlus * 3, width, height), "Credits"))
+        if (GUI.Button(new Rect(left, top + topPlus * 3, width, height), "Credits", style))
         {
             Controls.renderer.enabled = false;
             if (Credits.renderer.isVisible)
@@ -42,7 +52,7 @@ public class MainMenu : MonoBehaviour
         {
             Application.LoadLevel("Settings");
         }*/
-        if (GUI.Button(new Rect(left, top + topPlus * 4, width, height), "Exit"))
+        if (GUI.Button(new Rect(left, top + topPlus * 4, width, height), "Exit", style))
         {
             Application.Quit();
         }
